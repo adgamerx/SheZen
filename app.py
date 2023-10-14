ad2b25435f0bd96998a49adafbad827bf620682b
adgamerx <amank.u.m100@gmail.com>
2023-10-15 00:42:05 +0530

    app.py updated

--- app.py
+++ app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/safeRoute')
+def safeRoute():
+    return render_template('safeRoute.html')
 if __name__ == '__main__':
     app.run(debug=True)
