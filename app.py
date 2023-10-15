730e13937229b6bea27da9c5df77421283e62881
Abhishek Verma <me@theabhishek.dev>
2023-10-15 11:00:30 +0530

    testing

--- app.py
+++ app.py
@@ -20,16 +20,15 @@ def home():
 
 @app.route('/register', methods=['POST'])
 def register():
-        user_data = request.get_json()
-        password = user_data["password"]
+        password = request.form["password"]
         verification_code = gen_string(20)
         user = {
-            "name": user_data["name"],
-            "pass": password,
-            "phone": user_data["phone"],
-            "blood_group": user_data["blood_group"],
-            "age": user_data["age"],
-            "email": user_data["email"],
+            "name": request.form["name"],
+            "pass": request.form["password"],
+            "phone": request.form["phone"],
+            "blood_group": request.form["blood"],
+            "age": urequest.form["age"],
+            "email": request.form["email"],
             "emergency_con": user_data["emergency_con"],
             "verification_code": verification_code,
             "verified": False
@@ -43,11 +42,9 @@ def verify():
     verification_code = request.args.get("code")
     user = supabase.table('users').select('*').eq('verification_code', verification_code).execute()
     if user:
-        auth_key = gen_string(length)
-        supabase.table("users").update({"verified": True, "verification_code": "", "auth_key": auth_key}).eq('verification_code', verification_code).execute()
-        response = make_response(render_template('index.html'))
-        response.headers['auth_key'] = auth_key
-        return response, 200
+        supabase.table("users").update({"verified": True, "verification_code": ""}).eq('verification_code', verification_code).execute()
+        session['username'] = user.data['email']
+        return render_template("dashboard.html")
     return jsonify({"message": "email not verified"}), 401
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -55,59 +52,53 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        data = request.get_json()
-        email = data['email']
-        password = data['password']
+        email = request.form["username"]
+        password = request.form['password']
         user = supabase.table('users').select("*").eq("email",email).execute().data[0]
         if user['email'] == email and user['pass'] == password:
-            auth_key = gen_string(length)
-            response = make_response(render_template('index.html'))
-            response.headers['auth_key'] = auth_key
-            return response, 200
+            session['username'] = email
+            return render_template("index.html"), 200
         return 'Invalid credentials', 401
 
 @app.route('/edit-profile', methods=['POST'])
 def edit_profile():
-    auth_key = request.headers['auth_key']
-    if auth_key:
-        user_data = request.get_json()
+    if session.get('logged_in'):
+        email = session['username']
         password = user_data["password"]
         user = {
-                "name": user_data["name"],
-                "pass": password,
-                "phone": user_data["phone"],
-                "blood_group": user_data["blood_group"],
-                "age": user_data["age"],
-                "email": user_data["email"],
-                "emergency_con": user_data["emergency_con"]
+                "name": request.form['name'],
+                "pass": request.form['password'],
+                "phone": request.form['phone'],
+                "blood_group": request.form['blood'],
+                "age": request.form['age'],
+                "email": request.form['email'],
+                "emergency_con": request.form['emergency_con']
             }
-            auth_key = user_data['auth_key']
-        supabase.table("users").update(user).eq('auth_key', auth_key).execute()
+        supabase.table("users").update(user).eq('email', user_data["email"]).execute()
         return render_template("dashboard.html")
     return redirect("/login")
     
 @app.route('/delete-profile', methods=['POST'])
 def delete_profile():
-    auth_key = request.headers['auth_key']
-    if auth_key:
-        user_data = request.get_json()
-        data = supabase.table('users').delete().eq('auth_key', auth_key).execute()
-        return render_template("register.html")
+    if session.get('logged_in'):
+        email = session['username']
+        data = supabase.table('users').delete().eq('email', user_data["email"]).execute()
+        return redirect('/register')
     
 @app.route('/create-shop', methods=['POST'])
 def create_shop():
     try:
-        auth_key = request.headers['auth_key']
-        if auth_key:
-            data = request.get_json()
+        if session.get('logged_in'):
+            name = request.form['shopname']
+            desc = request.form['shopdesc']
+            uid = request.form['shopid']
             shop_data = {
-                "name": data['name'],
-                "category": data['category'],
-                "uid": data['uid'],
-                "products": []
+                "name": name,
+                "desc": desc,
+                "uid": uid
             }
-            supabase.table("users").select("*").eq("auth_key", auth_key).execute()
-            supabase.table("users").update({"shop": shop_data}).eq('auth_key', auth_key).execute()
+            email = session['username']
+            supabase.table("users").update({"shop": shop_data}).eq('email', session['username']).execute()
             return redirect('/products')
         return redirect('/login')
     except Exception as e:
@@ -115,9 +106,9 @@ def create_shop():
 
 @app.route('/<shopid>/<pid>', methods=['GET'])
 def show_product(shopid, pid):
-    auth_key = request.headers['auth_key']
-    if auth_key:
-        user_shop = supabase.table("users").select("*").eq("auth_key", auth_key).execute().data[0]['shop']
+    if session.get('logged_in'):
+        email = session['username']
+        user_shop = supabase.table("users").select("*").eq("email", email).execute().data[0]['shop']
         if user_shop:
             product = user_shop['products'][pid-1]
             return render_template('product.html', product=product)
@@ -127,49 +118,28 @@ def show_product(shopid, pid):
 
 @app.route('/products', methods=['GET'])
 def products():
-    auth_key = request.headers['auth_key']
-    if auth_key:
+    if session.get('logged_in'):
         email = session['username']
-        data = supabase.table("users").select("*").eq("auth_key", auth_key).execute()
-        data = data.data[0]
+        businesses = []
+        data = supabase.table("users").select("*").execute().data
+        for user in data:
+            businesses.append(user['shop'])
         if data['shop']:
             products = data['shop']['products']
             return render_template('products.html', products=products)
         return redirect('/create-shop')
     return redirect('/login')
 
-@app.route('/add-product', methods=['POST'])
-def add_product():
-    auth_key = request.headers['auth_key']
-    if auth_key:
-        product_data = request.get_json()
-        data = supabase.table("users").select("*").eq("auth_key", auth_key).execute().data[0]
-        if data['shop']:
-            pid = len(data) + 1
-            product = {
-                "name": product_data['name'],
-                "description": product_data['description'],
-                "pid": pid,
-                "images": product_data['images'],
-                "category": product_data['category'],
-                "price": product_data['price']
-            }
-            data['products'].append(product)
-            supabase.table("users").update({"shop": data}).eq('auth_key', auth_key).execute()
-            return redirect('/products')
-        return redirect('/create-shop')
-    return redirect('/login')
-
 @app.route('/logout')
 def logout():
+    session.clear()
     return redirect('/login')
 
 @app.route('/add-thread', methods=['POST'])
 def add_thread():
-    auth_key = request.headers['auth_key']
-    if auth_key:
+    if session.get('logged_in'):
         user_data = request.get_json()
-        user = supabase.table('users').select("*").eq('auth_key', auth_key).execute().data[0]['name']
+        user = supabase.table('users').select("*").eq('email', session['username']).execute().data[0]['name']
         thread = {
             'title': user_data['title'],
             'name': user_data['name'],
@@ -181,37 +151,34 @@ def add_thread():
 
 @app.route('/thread/<id>')
 def show_thread(id):
-    auth_key = request.headers['auth_key']
-    if auth_key:
+    if session.get('logged_in'):
         threads = supabase.table('forum').select('*').eq('id', id).execute().data[0]
         return render_template('thread.html', threads=threads)
     return redirect('/login')
 
 @app.route('/forum')
 def forum():
-    auth_key = request.headers['auth_key']
-    if auth_key:
+    if session.get('logged_in'):
         forum = supabase.table('forum').select('*').execute().data
         return render_template('forum.html', forum=forum)
     return redirect('/login')
 
 @app.route('/add-reply')
 def add_reply():
-    auth_key = request.headers['auth_key']
-    if auth_key:
+    if session.get('logged_in'):
         reply_text = request.form['reply']
         thread_id = request.form['id']
-        name = supabase.table('users').select("*").eq('auth_key', auth_key).execute().data[0]['name']
+        name = supabase.table('users').select("*").eq('email', session['username']).execute().data[0]['name']
         reply = {
             "reply": reply_text,
             "name": name
         }
         forum = supabase.table('forum').select('*').eq('id', thread_id).execute().data
         forum['replies'].append(reply)
-        supabase.table('forum').update(forum).eq('auth_key', auth_key).execute()
+        supabase.table('forum').update(forum).eq('email', email).execute()
         return redirect(f'/thread/{thread_id}')
         data['products'].append(product)
-        supabase.table("users").update({"shop": data}).eq('auth_key', auth_key).execute()
+        supabase.table("users").update({"shop": data}).eq('email', session['username']).execute()
         return redirect('/products')
     return redirect('/login')
 
--- static/app.js
+++ static/app.js
@@ -1,5 +1,24 @@
-document.getElementById("login_btn").addEventListener('click', () => {
-    let username = document.getElementById("username").value;
+document.getElementById("login_btn").addEventListener('click', async () => {
+    let email = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     
-})
+    const url = 'http://127.0.0.1:5000/login';
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "email": email, "password": password })
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        if (response.status === 200) {
+            const authKey = response.headers.get('auth_key');  // Retrieve the authentication key from the response headers
+            console.log('Authentication successful. Auth Key:', authKey);
+        } else {
+            console.error('Authentication failed');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
